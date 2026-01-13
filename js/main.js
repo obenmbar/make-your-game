a3f988d1c748a8e881b097e752ce853c63afd869
obenmbar <othmane.benmbarek.20@edu.uiz.ac.ma>
2026-01-13 06:41:58 +0100

    UPDATE

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+
 const gamearea = document.getElementById('game-area')
 const paddle = document.getElementById('paddle')
 const ball = document.getElementById('ball')
@@ -5,14 +6,59 @@ const brickcontainer = document.getElementById('bricks-container')
 const startmessege = document.getElementById('start-message')
 const scoredisplay = document.getElementById('score')
 
-const paddleSpeed = 8
+const paddleSpeed = 20
 let gameRunning = false
-let ballSpeedX = 4
-let ballSpeedY = 4
+let ballSpeedX = 2
+let ballSpeedY = -2
 let score = 0
-console.log('hadchi mzyan haliyan', paddle)
 
-document.addEventListener('keydown',event => {
-    console.log(event.key)
-    console.log(event)
+const gamewidth = gamearea.clientWidth
+let paddlewidth = paddle.offsetWidth
+let startposition = (gamewidth - paddlewidth) / 2
+let currentX = startposition
+paddle.style.transform = `translateX(${startposition}px)`
+// controle de paddle 
+
+document.addEventListener('keydown', event => {
+
+
+    if (event.key === 'ArrowRight') {
+        let position = currentX + paddleSpeed
+        currentX = position
+        if (position < gamewidth - paddlewidth) {
+            paddle.style.transform = `translateX(${position}px)`
+        } else {
+            currentX = gamewidth - paddlewidth
+            paddle.style.transform = `translateX(${currentX}px)`
+
+        }
+
+    } else if (event.key === 'ArrowLeft') {
+        if (currentX - paddleSpeed > 0) {
+            var position = currentX - paddleSpeed
+        } else {
+            position = 0
+        }
+        currentX = position
+        paddle.style.transform = `translateX(${position}px)`
+    }
+    moveBallWithPaddle()
 })
+
+let gameheight = gamearea.clientHeight
+let ballwidth = ball.offsetWidth
+let paddleheight = paddle.clientHeight
+let ballX = 0
+let ballY = gameheight - paddleheight - ballwidth -20
+
+function moveBallWithPaddle(){
+    ballX =  currentX + paddlewidth/2 - ballwidth/2
+    ball.style.transform = `translate(${ballX}px, ${ballY}px)`;
+}
+
+moveBallWithPaddle()
+
+
+ function move(){
+    
+ }
