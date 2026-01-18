735d90ec57e9ad81533c33be9f4d1440ee6bcc1b
obenmbar <othmane.benmbarek.20@edu.uiz.ac.ma>
2026-01-18 08:54:48 +0100

    structurer

---
+++ js/ui.js
@@ -0,0 +1,35 @@
+import { state } from './gameState.js';
+
+const timerDisplay = document.getElementById('timer');
+const livesDisplay = document.getElementById('lives');
+const scoreDisplay = document.getElementById('score');
+const startMessage = document.getElementById('start-message');
+
+export function updateTimer() {
+    if (state.gameRunning) {
+        state.timerSecond++;
+    }
+    let minutes = Math.floor(state.timerSecond / 60);
+    let seconds = state.timerSecond % 60;
+     let formattedMin = minutes < 10 ? `0${minutes}` : minutes;
+    let formattedSec = seconds < 10 ? `0${seconds}` : seconds;
+    timerDisplay.innerText = `${formattedMin}:${formattedSec}`;
+}
+
+export function displayLives() {
+    const gokuLive = '<img src="../images/lives.png" width="30" height="30" style="margin-right: 5px;">';
+    livesDisplay.innerHTML = gokuLive.repeat(state.lives);
+}
+
+export function updateScore() {
+    scoreDisplay.innerText = state.score;
+}
+
+export function showMessage(text) {
+    startMessage.innerText = text;
+    startMessage.style.display = 'block';
+}
+
+export function hideMessage() {
+    startMessage.style.display = 'none';
+}
