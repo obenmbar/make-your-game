e15b4836d113c53df5cd360bdac686e7ec64907b
obenmbar <othmane.benmbarek.20@edu.uiz.ac.ma>
2026-01-18 08:54:17 +0100

    structurer

---
+++ js/gameState.js
@@ -0,0 +1,13 @@
+export const state = {
+    score: 0,
+    lives: 3,
+    ballX:0,
+    ballY:0,
+    gameRunning: false,
+    isResetting: false,
+    rightPressed: false,
+    leftPressed: false,
+    timerSecond: 0,
+    currentPaddleX: 0,
+    bricks: [],
+};
